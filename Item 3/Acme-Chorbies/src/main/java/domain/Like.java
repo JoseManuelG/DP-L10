e2c13c3da3ac3e719542c9564115db6db18e687c
Javi <trarsheise@gmail.com>
2017-03-29 21:01:41 +0200

    Añadida relación unica en Like

--- Item 3/Acme-Chorbies/src/main/java/domain/Like.java
+++ Item 3/Acme-Chorbies/src/main/java/domain/Like.java
@@ -7,8 +7,10 @@ import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -19,6 +21,11 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Access(AccessType.PROPERTY)
+@Table(uniqueConstraints = {
+	@UniqueConstraint(columnNames = {
+		"liker", "liked"
+	})
+})
 public class Like extends DomainEntity {
 
 	// Attributes -------------------------------------------------------------
