16c99372f684ec19328b85b3fe5824b6cd6b6567
Javi <trarsheise@gmail.com>
2017-03-29 19:46:45 +0200

    Cambiado a chorbies

--- Item 3/Acme-Chorbi/src/main/java/domain/Chorbi.java
+++ Item 3/Acme-Chorbi/src/main/java/domain/Chorbi.java
@@ -4,23 +4,23 @@ package domain;
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
-import javax.persistence.Index;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Entity
 @Access(AccessType.PROPERTY)
-@Table(indexes = {
-	@Index(columnList = "status")
-})
-public class Application extends DomainEntity {
+public class Chorbi extends DomainEntity {
 
 	// Attributes -------------------------------------------------------------
 
 	private String	status;
+	private String	status;
+	private String	status;
+	private Date	status;
+	private String	status;
+	private boolean	status;
 
 
 	@Pattern(regexp = "^PENDING$|^ACCEPTED$|^DENIED$")
