8949700231b058dfaf2173ffc892a491e58e6481
Javi <trarsheise@gmail.com>
2017-03-30 20:01:56 +0200

    Repository dashboard

--- Item 3/Acme-Chorbies/src/main/java/repositories/DashboardRepository.java
+++ Item 3/Acme-Chorbies/src/main/java/repositories/DashboardRepository.java
@@ -14,57 +14,78 @@ import domain.Chorbi;
 public interface DashboardRepository extends JpaRepository<Administrator, Integer> {
 
 	// Dashboard - 01
-	@Query(name = "select count(c), c.coordinates.country from Chorbi c group by c.coordinates.country")
+	@Query("select count(c), c.coordinates.country from Chorbi c group by c.coordinates.country")
 	public List<Object[]> numberOfChorbiesPerCountry();
 
 	// Dashboard - 01
-	@Query(name = "select count(c), c.coordinates.city from Chorbi c group by c.coordinates.city")
+	@Query("select count(c), c.coordinates.city from Chorbi c group by c.coordinates.city")
 	public List<Object[]> numberOfChorbiesPerCity();
 
 	// Dashboard - 02	
-	@Query(name = "select min(DATEDIFF(CURRENT_DATE, c.birthDate)/365.256363004) from Chorbi c")
+	@Query("select min(DATEDIFF(CURRENT_DATE, c.birthDate)/365.256363004) from Chorbi c")
 	public Integer minAgeOfChorbies();
 
 	// Dashboard - 02	
-	@Query(name = "select max(DATEDIFF(CURRENT_DATE, c.birthDate)/365.256363004) from Chorbi c")
+	@Query("select max(DATEDIFF(CURRENT_DATE, c.birthDate)/365.256363004) from Chorbi c")
 	public Integer maxAgeOfChorbies();
 
 	// Dashboard - 02	
-	@Query(name = "select avg(DATEDIFF(CURRENT_DATE, c.birthDate)/365.256363004) from Chorbi c")
+	@Query("select avg(DATEDIFF(CURRENT_DATE, c.birthDate)/365.256363004) from Chorbi c")
 	public Double averageAgeOfChorbies();
 
 	// Dashboard - 03
-	@Query(name = "select count(c) from CreditCard c where c.expirationYear > YEAR(CURRENT_DATE) and c.expirationMonth >= MONTH(CURRENT_DATE)")
+	@Query("select count(c) from CreditCard c where c.expirationYear > YEAR(CURRENT_DATE) and c.expirationMonth >= MONTH(CURRENT_DATE)")
 	public Double countValidCreditCard();
 
 	// Dashboard - 04
-	@Query(name = "select count(c) from Chorbi c where c.desiredRelationship='activities'")
+	@Query("select count(c) from Chorbi c where c.desiredRelationship='activities'")
 	public Double countChorbiesWhoDesireActivities();
 
 	// Dashboard - 04
-	@Query(name = "select count(c) from Chorbi c where c.desiredRelationship='friendship'")
+	@Query("select count(c) from Chorbi c where c.desiredRelationship='friendship'")
 	public Double countChorbiesWhoDesireFriendship();
 
 	// Dashboard - 04
-	@Query(name = "select count(c) from Chorbi c where c.desiredRelationship='love'")
+	@Query("select count(c) from Chorbi c where c.desiredRelationship='love'")
 	public Double countChorbiesWhoDesireLove();
 
 	// Dashboard - 05
-	@Query(name = "select c from Likes l, Chorbi c where c=l.liked group by c order by count(c) desc")
+	@Query("select c from Likes l, Chorbi c where c=l.liked group by c order by count(c) desc")
 	public List<Chorbi> findChorbiesOrderedByLikes();
 
 	// Dashboard - 05
-	@Query(name = "select c from Chorbi c where c not in (select distinct l.liked from Likes l)")
+	@Query("select c from Chorbi c where c not in (select distinct l.liked from Likes l)")
 	public List<Chorbi> findChorbiesWithoutLikes();
 
 	//Dashboard - 06
-	@Query(name = "select count(l.liked) from Likes l group by l.liked order by count(l.liked) asc")
-	public Double minNumberOfLikesPerChorbi();
-	//se hace un count de chorbies y si es mayor que el size de la query de arriba, el minimo es 0, si no es el primer elemento que devuelve
+	@Query("select count(l.liked) from Likes l group by l.liked order by count(l.liked) asc")
+	public List<Double> minNumberOfLikesPerChorbi();
 
 	//Dashboard - 06
-	@Query(name = "select count(l.liked) from Likes l group by l.liked order by count(l.liked) desc")
-	public Double maxNumberOfLikesPerChorbi();
-	//el maximo es el primer elemento que devuelve
+	@Query("select count(l.liked) from Likes l group by l.liked order by count(l.liked) desc")
+	public List<Double> maxNumberOfLikesPerChorbi();
 
+	//Dashboard - 07
+	@Query("select count(m) from Chirp m where m.isSender=false and m.recipient is not null")
+	public Double averageOfChirpsReceivedPerChorbi();
+
+	//Dashboard - 07
+	@Query("select count(m) from Chirp m where m.isSender=false and m.recipient is not null group by m.recipient.id order by count(m) asc")
+	public List<Double> minOfChirpsReceivedPerChorbi();
+
+	//Dashboard - 07
+	@Query("select count(m) from Chirp m where m.isSender=false and m.recipient is not null group by m.recipient.id order by count(m) desc")
+	public List<Double> maxOfChirpsReceivedPerChorbi();
+
+	//Dashboard - 08
+	@Query("select count(m) from Chirp m where m.isSender=true and m.sender is not null")
+	public Double averageOfChirpsSentPerChorbi();
+
+	//Dashboard - 08
+	@Query("select count(m) from Chirp m where m.isSender=true and m.sender is not null group by m.sender.id order by count(m) asc")
+	public Double minOfChirpsSentPerChorbi();
+
+	//Dashboard - 08
+	@Query("select count(m) from Chirp m where m.isSender=true and m.sender is not null group by m.sender.id order by count(m) desc")
+	public Double maxOfChirpsSentPerChorbi();
 }
