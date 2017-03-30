56b490d01212c41babcaf58971f8cbf79db4e5f3
Daniel Iglesias Pérez <daniglper@gmail.com>
2017-03-30 20:01:39 +0200

    dashboard services

--- Item 3/Acme-Chorbies/src/main/java/services/DashboardService.java
+++ Item 3/Acme-Chorbies/src/main/java/services/DashboardService.java
@@ -1,6 +1,7 @@
 
 package services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import repositories.ChorbiRepository;
 import repositories.DashboardRepository;
+import repositories.LikesRepository;
 import domain.Chorbi;
 
 @Service
@@ -21,6 +23,9 @@ public class DashboardService {
 
 	@Autowired
 	private ChorbiRepository	chorbiRepository;
+
+	@Autowired
+	private LikesRepository		likesRepository;
 
 
 	//Dashboard - 01
@@ -110,10 +115,62 @@ public class DashboardService {
 
 	//Dashboard - 05
 	public List<Chorbi> chorbiesOrderedByLikes() {
-		List<Chorbi> res;
+		final List<Chorbi> res;
 
-		res = this.dashboardRepository.findChorbiesOrderedByLikes();
+		res = new ArrayList<Chorbi>();
+		res.addAll(this.dashboardRepository.findChorbiesOrderedByLikes());
 		res.addAll(this.dashboardRepository.findChorbiesWithoutLikes());
+
+		return res;
+	}
+
+	//Dashboard - 06
+	public Integer minNumberOfLikesPerChorbi() {
+		Long chorbies;
+		List<Double> counts;
+		Double res;
+
+		counts = new ArrayList<Double>();
+
+		chorbies = this.chorbiRepository.count();
+		counts.addAll(this.chorbiRepository.minNumberOfLikesPerChorbi());
+
+		if (chorbies > 0 && counts.size() == chorbies)
+			res = counts.get(0);
+		else
+			res = 0.;
+
+		return res.intValue();
+	}
+
+	//Dashboard - 06
+	public Integer maxNumberOfLikesPerChorbi() {
+		List<Double> counts;
+		Double res;
+
+		counts = new ArrayList<Double>();
+		counts.addAll(this.chorbiRepository.maxNumberOfLikesPerChorbi());
+
+		if (counts.size() > 0)
+			res = counts.get(0);
+		else
+			res = 0.;
+
+		return res.intValue();
+	}
+
+	//Dashboard - 06
+	public Double avgNumberOfLikesPerChorbi() {
+		Long chorbies, likes;
+		Double res;
+
+		chorbies = this.chorbiRepository.count();
+		likes = this.likesRepository.count();
+
+		if (chorbies > 0)
+			res = 1.0 * likes / chorbies;
+		else
+			res = 0.;
 
 		return res;
 	}
